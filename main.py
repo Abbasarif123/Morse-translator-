a5e957efc9a84cd5179f038b23c84c3cafa53ed0
DrMemezo <nyanisbusy@gmail.com>
2025-11-01 15:52:12 +0500

    idk

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src.inputs import get_input
 
 def main():
     text = get_input()
+    # TODO: Add translation functions here
+    # TODO: Add file output + pretty CLI output
     print(text)
 
 
